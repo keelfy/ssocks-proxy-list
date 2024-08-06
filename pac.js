1e0581dbe91eb2e2f77e1b6a180d646e20726230
Egor Kuzmin <tehnyla@gmail.com>
2024-08-06 15:13:17 +0200

    Update pac.js

--- pac.js
+++ pac.js
@@ -931,7 +931,9 @@ function FindProxyForURL(url, host) {
   // remove leading www
   shost = shost.replace(/^www\.(.+)/, "$1");
 
-  fbtw = ['twitter.com', 'twimg.com', 't.co', 'x.com', '2ip.ru', 'googlevideo.com',
+  fbtw = ['twitter.com', 'twimg.com', 't.co', 'x.com', 
+          '2ip.ru', 
+          'googlevideo.com',
           'facebook.com', 'fbcdn.net',
           'instagram.com', 'cdninstagram.com',
           'fb.com', 'messenger.com',
